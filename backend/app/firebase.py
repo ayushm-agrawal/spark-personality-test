49e0da9d38bc2519ded415b3eb9e29d8a9ae7744
Ayush Manish Agrawal <ayush.agrawal7661@gmail.com>
2025-03-19 16:30:25 -0400

    move firebase credentials to .env

--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -1,8 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import json
+import os
+from dotenv import load_dotenv
 
-cred = credentials.Certificate("../serviceAccountKey.json")
+load_dotenv()
+
+service_account_key = os.getenv("SERVICE_ACCOUNT_KEY")
+if not service_account_key:
+    raise ValueError("SERVICE_ACCOUNT_KEY environment variable not set.")
+
+key_dict = json.loads(service_account_key)
+cred = credentials.Certificate(key_dict)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
